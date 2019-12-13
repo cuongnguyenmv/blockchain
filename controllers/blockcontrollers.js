06cb7854b54efb45363a1031431f1eb9db010f0b
root <root@localhost.localdomain>
2019-12-13 10:28:15 +0700

    blockchain

--- controllers/blockcontrollers.js
+++ controllers/blockcontrollers.js
@@ -1,31 +1,73 @@
 const crypto = require('crypto')
-
-
-function ValidHashDifficulty(hash,difficulty = 4){
-	console.log(hash)
-	for(var i= 0 ; i < 1000 ; i++){
-		if(hash[i] !== '0'){
-			break;
-		}
+const EC = require('elliptic').ec
+const ec = new EC('secp256k1')
+const key = ec.genKeyPair()
+const publicKey = key.getPublic('hex')
+const privateKey1 = key.getPrivate('hex')
+const privateKey2 = key.getPrivate('hex')
+console.log('public key' + publicKey)
+console.log('privateKey '+ privateKey1)
+console.log('privateKey '+ privateKey2)
+class Transaction{
+	constructor(fromAddress,toAddress,amount){
+		this.fromAddress = fromAddress
+		this.toAddress = toAddress
+		this.amount = this.amount
 	}
-	return i >= difficulty ;
 }
-
-exports.pow = function(){
-	var data = '200000'
-	var nonce =  0;
-	var hash;
-	var input;
-	var index
-	var preHash;
-	var timetamp;
-	while(!ValidHashDifficulty(hash)){
-		nonce = nonce + 1
-		input = input + preHash + timetamp + data + nonce
-		hash = crypto.createHmac('sha256','12312321')
-                   .update(input)
+class Block{
+	constructor(index, timetamp,data,preHash =""){
+		this.index = index
+		this.timetamp = timetamp
+		this.data =data
+		this.preHash = preHash
+		this.hash = this.calculateHash()
+		this.nonce = 0
+		
+	}
+	calculateHash(){
+		return crypto.createHmac('sha256','12312321')
+                   .update(this.index + this.preHash + this.timetamp + JSON.stringify(this.data) + this.nonce)
                    .digest('hex')
 	}
-	console.log(hash)
-	return hash;
+	mineBlock(difficulty){
+		while(String(this.hash).substring(0,difficulty) !== Array(difficulty + 1).join('0')){
+			this.nonce++
+			this.hash = this.calculateHash()
+		}
+		console.log("Block mined: "+ this.hash)
+	}
 }
+class Blockchain{
+	constructor(){
+		this.chain = [this.createGenesisBlock()]
+		this.difficulty = 4
+	}
+	createGenesisBlock(){
+		return new Block(0,'01/01/2017','Geneis block',"0")
+	}
+	getLastestBlock(){
+		return this.chain[this.chain.length -1]
+	}
+	addBlock(newBlock){
+		newBlock.preHash = this.getLastestBlock().hash
+		newBlock.mineBlock(this.difficulty)
+		this.chain.push(newBlock)
+	}
+	isChainValid(){
+		for(var i = 1; i < this.chain.length; i++){
+			const curBlock = this.chain[i]
+			const preBlock = this.chain[i-1]
+			if(curBlock.hash !== curBlock.calculateHash())
+			{
+				return false;
+			}
+			if(curBlock.preHash !== preBlock.hash)
+				return false
+		}
+		return true
+	}
+}
+
+let coin = new Blockchain()
+coin.addBlock(new Block(1,'10/12/2017',{amount:4}))
